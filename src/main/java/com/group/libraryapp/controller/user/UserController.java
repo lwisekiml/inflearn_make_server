dae8019a58a537ffa1bef5890b32b50f39a6139b
wisekim <lwisekiml@naver.com>
2023-04-22 21:33:36 +0900

    섹션 1. 생애 최초 API 만들기 : 8강. 유저 조회 API 개발과 테스트

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -2,6 +2,8 @@ package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.dto.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,19 @@ public class UserController {
 
     private final List<User> users = new ArrayList<>();
 
+
     @PostMapping("/user")
     public void addUser(@RequestBody UserCreateRequest request) {
         users.add(new User(request.getName(), request.getAge()));
+    }
+
+    @GetMapping("/user")
+    public List<UserResponse> getUser() {
+        List<UserResponse> responses = new ArrayList<>();
+        for (int i = 0; i < users.size(); i++) {
+//            responses.add(new UserResponse(i + 1, users.get(i).getName(), users.get(i).getAge()));
+            responses.add(new UserResponse(i + 1, users.get(i)));
+        }
+        return responses;
     }
 }
