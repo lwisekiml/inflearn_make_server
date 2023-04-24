cd23d129947eccec0ee26f11d6f71cd681fcbbdc
wisekim <lwisekiml@naver.com>
2023-04-24 17:20:52 +0900

    15강 설명 추가

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -4,8 +4,11 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @RestController
@@ -26,6 +29,10 @@ public class UserController {
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
         String sql = "SELECT * FROM user";
+
+//        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
+//            @Override
+//            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
         return jdbcTemplate.query(sql, (rs, rowNum) -> {
             long id = rs.getLong("id");
             String name = rs.getString("name");
@@ -37,8 +44,11 @@ public class UserController {
     @PutMapping("/user")
     public void updateUser(@RequestBody UserUpdateRequest request) {
         String readSql = "SELECT * FROM user WHERE id = ?";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
-        if (isUserNotExist) {
+        // jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId())
+        // user가 존재하면 List에 0이 있고, user가 존재하지 않는다면 빈 List가 반환된다.
+        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId())
+                                                .isEmpty();
+        if (isUserNotExist) { // user가 존재하지 않는다면
             throw new IllegalArgumentException();
         }
 
