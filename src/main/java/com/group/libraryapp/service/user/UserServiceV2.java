f3a5bfe6e07d4bbd59c321f1f21f1b2b3c8b1f8c
wisekim <lwisekiml@naver.com>
2023-04-28 22:08:49 +0900

    생애 최초 JPA 사용하기 : 28강. 트랜잭션 적용과 영속성 컨텍스트

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,13 +20,19 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
+    // 아래 있는 함수가 시작될 때 start transaction;을 해준다(트랜잭션을 시작!)
+    // 함수가 예외 없이 잘 끝났다면 commit
+    // 혹시라도 문제가 있다면 rollback
+    @Transactional
     public void saveUser(UserCreateRequest request) {
         userRepository.save(new User(request.getName(), request.getAge())); // User 객체 반환
+        throw new IllegalArgumentException();
 //        User user = userRepository.save(new User(request.getName(), request.getAge()));
 //        System.out.println("user.getId() = " + user.getId());
 
     }
 
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers() {
         List<User> users = userRepository.findAll();
         return users.stream()
@@ -34,14 +41,16 @@ public class UserServiceV2 {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
     public void updateUser(UserUpdateRequest request) {
         User user = userRepository.findById(request.getId())
                 .orElseThrow(IllegalArgumentException::new); // User 가 비어있는 경우 에러를 던짐
 
         user.updateName(request.getName());
-        userRepository.save(user);
+//        userRepository.save(user); // 영속성 컨텍스트 - 변경감지
     }
 
+    @Transactional
     public void deleteUser(String name) {
         // SELECT * FROM user WHERE name = ?
         User user = userRepository.findByName(name)
