045ff63fda9c0f3a845dc5ba01b9184729bd945b
wisekim <lwisekiml@naver.com>
2023-04-28 15:17:13 +0900

    생애 최초 JPA 사용하기 : 26강. By 앞에 들어갈 수 있는 구절 정리 - find

--- src/main/java/com/group/libraryapp/dto/domain/user/UserRepository.java
+++ src/main/java/com/group/libraryapp/dto/domain/user/UserRepository.java
@@ -2,11 +2,13 @@ package com.group.libraryapp.dto.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     // 유저가 없다면 null 반환
     // SELECT * FROM user WHERE name = ?
-    User findByName(String name);
+    Optional<User> findByName(String name);
 
 
 }
--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -44,10 +44,8 @@ public class UserServiceV2 {
 
     public void deleteUser(String name) {
         // SELECT * FROM user WHERE name = ?
-        User user = userRepository.findByName(name);
-        if (user == null) {
-            throw new IllegalArgumentException();
-        }
+        User user = userRepository.findByName(name)
+                .orElseThrow(IllegalArgumentException::new);
 
         userRepository.delete(user);
     }
