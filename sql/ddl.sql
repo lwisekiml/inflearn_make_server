f24e2bd433a85f65645874d904e1f99e11263f9a
wisekim <lwisekiml@naver.com>
2023-05-02 18:50:59 +0900

    빼먹은 user_loan_history 쿼리

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -65,3 +65,13 @@ create table book (
 show tables;
 
 select * from book;
+
+create table user_loan_history (
+    id bigint auto_increment,
+    user_id bigint,
+    book_name varchar(255),
+    is_return tinyint(1),
+    primary key (id)
+);
+
+select * from user_loan_history;
