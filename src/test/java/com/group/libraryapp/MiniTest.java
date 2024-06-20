ef69be8d803439fc9260e979e23b72bf232065ab
wisekim <lwisekiml@naver.com>
2024-06-20 21:43:36 +0900

    MiniTest

---
+++ src/test/java/com/group/libraryapp/MiniTest.java
@@ -0,0 +1,33 @@
+package com.group.libraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+@SpringBootTest
+public class MiniTest {
+
+    @Test
+    public void test() {
+        String targetDate = "2020-02-02";
+
+//        YearMonth yearMonth = YearMonth.from(LocalDate.parse(targetDate, DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        YearMonth yearMonth = YearMonth.from(LocalDate.now());
+
+        System.out.println(yearMonth.lengthOfMonth());
+
+
+        LocalDateTime startTime = LocalDateTime.of(2024, 6, 20, 10, 35);
+// 결과 : 17:14:55
+        LocalDateTime endTime = LocalDateTime.of(2024, 6,20,17,35);
+// 결과 : 18:17:35
+
+        Duration duration = Duration.between(startTime, endTime);
+        long minutes = duration.toMinutes();
+        duration.getSeconds();
+
+
+    }
+}
